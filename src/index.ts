3d163f65e8d2ccedafcc8e77f79672a7bb8123e6
Roz <roz@rjmunhoz.me>
2022-01-14 19:55:31 -0300

    feat: better error messages

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as ts from 'typescript';
+import path from 'path';
 
 export const install = (options: ts.CompilerOptions = {}) => {
   expect.extend({
@@ -14,18 +15,63 @@ export const install = (options: ts.CompilerOptions = {}) => {
         (diagnostic) => diagnostic.category === ts.DiagnosticCategory.Error
       );
 
-      const successResult = {
-        pass: true,
-        message: () => `Expected ${filePath} to produce a compile error`,
-      };
+      const expandTip = this.expand
+        ? ''
+        : '\n\nUse --expand to show full file paths';
 
-      const errorResult = {
+      if (!errorDiagnostics) {
+        return {
+          pass: true,
+          message: () => {
+            const fileName = this.expand ? filePath : path.basename(filePath);
+
+            const hint = this.utils.matcherHint(
+              'toCompile',
+              `'${fileName}'`,
+              '',
+              {
+                isNot: this.isNot,
+              }
+            );
+
+            const expected = this.utils.printExpected(
+              `${fileName} produces a compilation error`
+            );
+            const received = this.utils.printReceived(
+              `${fileName} compiled successfully`
+            );
+
+            return `${hint}\n\nExpected: ${expected}\nReceived: ${received}${expandTip}`;
+          },
+        };
+      }
+
+      return {
         pass: false,
-        message: () =>
-          `Expected ${filePath} to be compiled successfully, but got ${errorDiagnostics?.messageText}.`,
-      };
+        message: () => {
+          const message = errorDiagnostics.messageText.toString();
+          const file = errorDiagnostics.file;
+          const fileName = this.expand
+            ? file?.fileName || ''
+            : path.basename(file?.fileName || '');
 
-      return errorDiagnostics ? errorResult : successResult;
+          const lineAndChar = file?.getLineAndCharacterOfPosition(
+            errorDiagnostics.start || 0
+          );
+
+          const line = (lineAndChar?.line || 0) + 1;
+          const character = (lineAndChar?.character || 0) + 1;
+
+          const hint = this.utils.matcherHint('toCompile', `'${fileName}'`, '');
+
+          const expected = this.utils.printExpected('No compilation errors');
+          const received = this.utils.printReceived(
+            `${message} (${fileName}:${line}:${character})`
+          );
+
+          return `${hint}\n\nExpected: ${expected}\nReceived: ${received}${expandTip}`;
+        },
+      };
     },
   });
 };
