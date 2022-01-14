1ca01601714d82f63c9789bc1c25d089c12fe369
Roz <roz@rjmunhoz.me>
2022-01-14 19:13:21 -0300

    fix: remove program cache

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
 import * as ts from 'typescript';
 
-const programs: Record<string, ts.Program> = {};
-
 export const install = (options: ts.CompilerOptions = {}) => {
   expect.extend({
     toCompile(filePath: string) {
-      if (!programs[filePath]) {
-        programs[filePath] = ts.createProgram([filePath], {
-          maxNodeModuleJsDepth: 1,
-          ...options,
-        });
-      }
-
-      const program = programs[filePath];
+      const program = ts.createProgram([filePath], {
+        maxNodeModuleJsDepth: 1,
+        ...options,
+      });
 
       const diagnosticts = ts.getPreEmitDiagnostics(program);
 
